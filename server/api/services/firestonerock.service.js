55274bb28a64c93db1ce005d641287c9220b9a47
true2blue <842958037@qq.com>
2024-01-12 22:13:14 +0800

    fix heart beat

--- server/api/services/firestonerock.service.js
+++ server/api/services/firestonerock.service.js
@@ -17,14 +17,21 @@ class FireStoneRockService {
             'method': 'POST',
             'headers': {
                 'Accept': 'application/json, text/javascript, */*; q=0.01',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8',
                 'Connection': 'keep-alive',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Content-Length': 0,
                 'Host': 'mncg.10jqka.com.cn',
-                'Referer': 'http://mncg.10jqka.com.cn/cgiwt/index/index',
+                'Origin': 'https://mncg.10jqka.com.cn',
+                'Referer': 'https://mncg.10jqka.com.cn/cgiwt/index/index',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'X-Requested-With': 'XMLHttpRequest',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+                'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"'
             }
         };
         this.dfcf_options = {
