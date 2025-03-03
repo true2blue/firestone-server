a69a4791b2aa8c9d6eeff9a1d204eadb1b1e5b8a
Benjamin Li <842958037@qq.com>
2025-03-03 16:31:02 +0800

    add hgddm

--- server/api/models/config.js
+++ server/api/models/config.js
@@ -1,37 +1,40 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import l from '../../common/logger';
 
 const configSchema = new mongoose.Schema({
-    maxBuyNum: {
-        type: Number
-    },
-    curBuyNum: {
-        type: Number,
-        default: 0
-    },
-    cookie: {
-        type: String
-    },
-    validatekey: {
-        type: String
-    },
-    gddm:{
-        type: String
-    },
-    monitor_concept: {
-        type: Array
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId
-    }
+  maxBuyNum: {
+    type: Number,
+  },
+  curBuyNum: {
+    type: Number,
+    default: 0,
+  },
+  cookie: {
+    type: String,
+  },
+  validatekey: {
+    type: String,
+  },
+  gddm: {
+    type: String,
+  },
+  hgddm: {
+    type: String,
+  },
+  monitor_concept: {
+    type: Array,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
 });
 
 configSchema.statics.findByUserId = async function (userId) {
-    l.info(`find the config ${userId}`);
-    return this.findOne({
-        userId: mongoose.Types.ObjectId(userId)
-    });
-}
+  l.info(`find the config ${userId}`);
+  return this.findOne({
+    userId: mongoose.Types.ObjectId(userId),
+  });
+};
 
 const Config = mongoose.model('Config', configSchema);
 
