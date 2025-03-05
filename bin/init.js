585384c4023b7975a517a9a69ea0f69e317f26a9
true2blue <842958037@qq.com>
2025-03-05 19:40:06 +0800

    init

--- bin/init.js
+++ bin/init.js
@@ -179,22 +179,22 @@ db.strategies.insertMany([{
             "end" : "15:00"
         },
         "force_sell_time" : "15:50:00",
-        "start_buy_line" : "3",
+        "start_buy_line" : "1",
         "buy_on_low_limit" : "1",
         "cb" : "0",
-        "buy_below_cb_percent" : "-999",
+        "buy_below_cb_percent" : "3",
         "min_rebound" : "0.5",
-        "max_rebound" : "3.0",
+        "max_rebound" : "1.0",
         "stop_win" : "3.0",
         "drop_from_high" : "0.5",
         "drop_percent" : "1.0",
         "speed" : {
             "time_2" : "1",
-            "percent" : "1"
+            "percent" : "0"
         },
         "money" : {
             "time" : "1",
-            "amount" : "60"
+            "amount" : "0"
         },
         "volume" : "1000",
         "delta" : 0
