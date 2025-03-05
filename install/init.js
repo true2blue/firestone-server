86b3c91a383d3f1f9108aec41e8bdd3ecacd35cf
true2blue <842958037@qq.com>
2025-03-05 18:19:41 +0800

    init

--- install/init.js
+++ install/init.js
@@ -190,6 +190,7 @@ db.strategies.insertMany([{
     max_rebound: '3.0',
     stop_win: '3.0',
     drop_from_high: '0.5',
+    drop_percent : '1.0',
     speed: {
       time_2: '1',
       percent: '1',
